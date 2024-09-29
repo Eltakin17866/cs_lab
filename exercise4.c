81f87e2a13eedf812a7bacadf5124d09bc846447
EltakinAliyev17866 <151494887+Eltakin17866@users.noreply.github.com>
2024-09-29 23:18:49 +0400

    Add files via upload

---
+++ exercise4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    float num, sum = 0.0;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter a float: ");
+        scanf("%f", &num);
+        sum += num;
+    }
+ printf("Sum of the floats: %.1f\n", sum);
+
+    return 0;
+}
+ 
